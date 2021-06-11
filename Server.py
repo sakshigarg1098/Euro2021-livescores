86bfeb8155e5120d674d16bbd28a61cd5255369e
sakshi <sakshi.garg1098@gmail.com>
2021-06-11 19:23:27 +0530

    adding flags

--- Server.py
+++ Server.py
@@ -1,5 +1,16 @@
 import time
+import tweepy
 from Match_Info import Football
+
+API_Key = 'Rl2iEuGVcttMr7XJpPZTfgUrb'
+API_Secret_Key = 'qW5qKmxnBAWfZhEnudivqLHkN0VVdAg62HB4VfNAr1QlDbZUA9'
+Access_Token = '1402255274984026114-VDnOyMDEdwRkWKgifQaPOlUx5lrkVQ'
+Access_Token_Secret = 'ZDO4XrLGLa72KvAgiG5wm8q8tukYQa56b7uYePTjapxiU'
+
+auth = tweepy.OAuthHandler(API_Key, API_Secret_Key)
+auth.set_access_token(Access_Token, Access_Token_Secret)
+
+api = tweepy.API(auth)
 
 previous_team1_events = []
 previous_team2_events = []
@@ -25,69 +36,67 @@ while True:
         if match2.team1_yellow() is True:
             if len(current_team1_scorers) > len(previous_team1_scorers):
                 previous_team1_scorers = current_team1_scorers
-                print('Yellow Card by : {} ({})'.format(match2.team1_scorers()[-1], match2.team1))
-                print(match2.current_score)
+                api.update_status('Yellow Card by : {} ({})'.format(match2.team1_scorers()[-1], match2.team1))
+                api.update_status(match2.current_score)
             else:
                 pass
         elif match2.team1_red() is True:
             if len(current_team1_scorers) > len(previous_team1_scorers):
                 previous_team1_scorers = current_team1_scorers
-                print('Red Card by : {} ({})'.format(match2.team1_scorers()[-1], match2.team1))
-                print(match2.current_score)
+                api.update_status('Red Card by : {} ({})'.format(match2.team1_scorers()[-1], match2.team1))
+                api.update_status(match2.current_score)
             else:
                 pass
         else:
             if len(current_team1_assists) > len(previous_team1_assists):
                 previous_team1_assists = current_team1_assists
-                print(match2.current_score)
-                print('Goal by : {} ({})'.format(match2.team1_scorers()[-1], match2.team1))
-                print('Assist: {}'.format(match2.team1_assists()[-1]))
-                print(match2.current_score)
+                api.update_status('Goal by : {} ({})'.format(match2.team1_scorers()[-1], match2.team1))
+                api.update_status('Assist: {}'.format(match2.team1_assists()[-1]))
+                api.update_status(match2.current_score)
             else:
                 try:
-                    print('Goal by : {} ({})'.format(match2.team1_scorers()[-1], match2.team1))
-                    print(match2.current_score)
+                    api.update_status('Goal by : {} ({})'.format(match2.team1_scorers()[-1], match2.team1))
+                    api.update_status(match2.current_score)
                 except:
-                    print(match2.current_score)
+                    api.update_status(match2.current_score)
 
     elif len(current_team2_events) > len(previous_team2_events):
         previous_team2_events = current_team2_events
         if match2.team2_yellow() is True:
             if len(current_team2_scorers) > len(previous_team2_scorers):
                 previous_team2_scorers = current_team2_scorers
-                print('Yellow Card by : {} ({})'.format(match2.team2_scorers()[-1], match2.team2))
-                print(match2.current_score)
+                api.update_status('Yellow Card by : {} ({})'.format(match2.team2_scorers()[-1], match2.team2))
+                api.update_status(match2.current_score)
             else:
                 pass
         elif match2.team2_red() is True:
             if len(current_team2_scorers) > len(previous_team2_scorers):
                 previous_team2_scorers = current_team2_scorers
-                print('Red Card by : {} ({})'.format(match2.team2_scorers()[-1], match2.team2))
-                print(match2.current_score)
+                api.update_status('Red Card by : {} ({})'.format(match2.team2_scorers()[-1], match2.team2))
+                api.update_status(match2.current_score)
             else:
                 pass
         else:
             if len(current_team2_assists) > len(previous_team2_assists):
                 previous_team2_assists = current_team2_assists
-                print(match2.current_score)
-                print('Goal by : {} ({})'.format(match2.team2_scorers()[-1], match2.team2))
-                print('Assist: {}'.format(match2.team2_assists()[-1]))
-                print(match2.current_score)
+                api.update_status('Goal by : {} ({})'.format(match2.team2_scorers()[-1], match2.team2))
+                api.update_status('Assist: {}'.format(match2.team2_assists()[-1]))
+                api.update_status(match2.current_score)
             else:
                 try:
-                    print('Goal by : {} ({})'.format(match2.team2_scorers()[-1], match2.team2))
-                    print(match2.current_score)
+                    api.update_status('Goal by : {} ({})'.format(match2.team2_scorers()[-1], match2.team2))
+                    api.update_status(match2.current_score)
                 except:
-                    print(match2.current_score)
+                    api.update_status(match2.current_score)
 
     else:
         if len(previous_times_lst) < len(current_times_lst):
             previous_times_lst = current_times_lst
             if current_times_lst[-1] == 'HT':
-                print('Half Time: {}'.format(match2.current_score))
+                api.update_status('Half Time: {}'.format(match2.current_score))
             elif current_times_lst[-1] == 'FT':
-                print('Full Time: {}'.format(match2.current_score))
-                print(match2.winner())
+                api.update_status('Full Time: {}'.format(match2.current_score))
+                api.update_status(match2.winner())
         else:
             pass
     time.sleep(3)
