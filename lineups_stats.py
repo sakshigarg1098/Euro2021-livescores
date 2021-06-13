9abc0073b87f1c31eeb6764983074d8540b10866
sakshi <sakshi.garg1098@gmail.com>
2021-06-13 21:43:07 +0530

    lineups and stats

---
+++ lineups_stats.py
@@ -0,0 +1,110 @@
+from requests_html import HTMLSession
+from datetime import datetime
+import re
+from Match_Info import Football
+
+m = Football('https://www.livescore.com/en/football/euro-2020/group-b/belgium-vs-russia/80736/')
+
+
+def lineups(goal_homepage):
+    session = HTMLSession()
+    matches = goal_homepage
+    res = session.get(matches)
+    match_data = res.html.find('.match-row-list', first=True)
+    details = match_data.find('[href^="/en-in/match/"]')
+    lineups_link_lst = []
+    for detail in details:
+        link = str(detail).split("'")[-2].split('/')
+        link.insert(4, 'lineups')
+        link[0] = 'https://www.goal.com'
+        link = '/'.join(link)
+        lineups_link_lst.append(link)
+    for link in lineups_link_lst:
+        if str(m.team1).lower() in re.split("-|/", link) and str(m.team2).lower() in re.split("-|/", link):
+            players = []
+            lineups = session.get(link).html.find('.widget-match-lineups__list.widget-match-lineups__starting-eleven .widget-match-lineups__name')
+            for player in lineups:
+                players.append(player.text)
+            players_team1 = players[:11]
+            players_team1_str = '\n'.join(players_team1)
+            players_team2 = players[11:]
+            players_team2_str = '\n'.join(players_team2)
+            api.update_status('Lineups ({}): \n{} \n\nLineups ({}): \n{}'.format(m.team1, players_team1_str, m.team2, players_team2_str))
+
+
+def stats(goal_homepage):
+    session = HTMLSession()
+    matches = goal_homepage
+    res = session.get(matches)
+    match_data = res.html.find('.match-row-list', first=True)
+    details = match_data.find('[href^="/en-in/match/"]')
+    stats_link_lst = []
+    for detail in details:
+        link = str(detail).split("'")[-2].split('/')
+        link.insert(4, 'commentary-result')
+        link[0] = 'https://www.goal.com'
+        link = '/'.join(link)
+        stats_link_lst.append(link)
+    for link in stats_link_lst:
+        if str(m.team1).lower() in re.split("-|/", link) and str(m.team2).lower() in re.split("-|/", link):
+            stats = session.get(link).html.find('.widget-match-stats .content')[0].text
+            possession = '{} - {} | {} - {}'.format(m.team1, stats.split()[1], m.team2, stats.split()[2])
+            shots_on_target = '{} - {} | {} - {}'.format(m.team1, stats.split()[11], m.team2, stats.split()[12])
+            total_passes = '{} - {} | {} - {}'.format(m.team1, stats.split()[15], m.team2, stats.split()[16])
+            api.update_status('Possession: {} \nShots on Target: {} \nTotal Passes: {}'.format(possession, shots_on_target, total_passes))
+
+
+print(stats('https://www.goal.com/en-in/results/2021-06-12'))
+
+
+def lineups_post_time(scheduled_time_str):
+    time_list = scheduled_time_str.split(':')
+    if int(time_list[1]) > 15:
+        if int(time_list[1]) < 25:
+            time_list[1] = '0' + str(int(time_list[1]) - 15)
+        else:
+            time_list[1] = str(int(time_list[1]) - 15)
+    else:
+        if time_list[0] == '00':
+            time_list[0] = '23'
+            time_list[1] = str(60 + (int(time_list[1]) - 15))
+        else:
+            time_list[1] = str(60 + (int(time_list[1]) - 15))
+            if int(time_list[0]) < 11:
+                time_list[0] = '0' + str(int(time_list[0]) - 1)
+            else:
+                time_list[0] = str(int(time_list[0]) - 1)
+    return ':'.join(time_list)
+
+
+def stats_post_time(scheduled_time_str):
+    time_list = scheduled_time_str.split(':')
+    if int(time_list[1]) < 30:
+        time_list[1] = str(int(time_list[1]) + 30)
+        if int(time_list[0]) < 9:
+            time_list[0] = '0' + str(int(time_list[0]) + 1)
+        else:
+            time_list[0] = str(int(time_list[0]) + 1)
+    else:
+        if time_list[0] == '22':
+            time_list[0] = '00'
+            if 29 < int(time_list[1]) < 40:
+                time_list[1] = '0' + str(int(time_list[1]) - 30)
+            else:
+                time_list[1] = str(int(time_list[1]) - 30)
+        elif time_list[0] == '23':
+            time_list[0] = '01'
+            if 29 < int(time_list[1]) < 40:
+                time_list[1] = '0' + str(int(time_list[1]) - 30)
+            else:
+                time_list[1] = str(int(time_list[1]) - 30)
+        else:
+            if int(time_list[0]) < 8:
+                time_list[0] = '0' + str((int(time_list[0]) + 2))
+            else:
+                time_list[0] = str((int(time_list[0]) + 2))
+            if 29 < int(time_list[1]) < 40:
+                time_list[1] = '0' + str(int(time_list[1]) - 30)
+            else:
+                time_list[1] = str(int(time_list[1]) - 30)
+    return ':'.join(time_list)
--- twitter.py
+++ twitter.py
@@ -1,13 +1,46 @@
-import tweepy
+from requests_html import HTMLSession
+from datetime import datetime, timedelta
+import re
 from Match_Info import Football
 
-API_Key = ''
-API_Secret_Key = ''
-Access_Token = ''
-Access_Token_Secret = ''
+session = HTMLSession()
+matches = 'https://www.goal.com/en-in/live-scores'
+res = session.get(matches)
+match_data = res.html.find('.match-row-list', first=True)
+details = match_data.find('[href^="/en-in/match/"]')
+m = Football('https://www.livescore.com/en/football/euro-2020/group-b/belgium-vs-russia/80736/')
+'''lineups_link_lst = []
+for detail in details:
+    link = str(detail).split("'")[-2].split('/')
+    link.insert(4, 'lineups')
+    link[0] = 'https://www.goal.com'
+    link = '/'.join(link)
+    lineups_link_lst.append(link)
+print(lineups_link_lst)
+for link in lineups_link_lst:
+    if str(m.team1).lower() in re.split("-|/", link) and str(m.team2).lower() in re.split("-|/", link):
+        if lineups_post_time == datetime.now().strftime('%H:%M'):
+            players = []
+            lineups = session.get(link).html.find('.widget-match-lineups__list.widget-match-lineups__starting-eleven .widget-match-lineups__name')
+            for player in lineups:
+                players.append(player.text)
+            players_team1 = players[:11]
+            players_team2 = players[11:]
+            print('{} Lineups: {} \n{} Lineups: {}'.format(m.team1, players_team1, m.team2, players_team2))'''
 
-auth = tweepy.OAuthHandler(API_Key, API_Secret_Key)
-auth.set_access_token(Access_Token, Access_Token_Secret)
-
-api = tweepy.API(auth)
+stats_link_lst = []
+for detail in details:
+    link = str(detail).split("'")[-2].split('/')
+    link.insert(4, 'commentary-result')
+    link[0] = 'https://www.goal.com'
+    link = '/'.join(link)
+    stats_link_lst.append(link)
+print(stats_link_lst)
+for link in stats_link_lst:
+    if str(m.team1).lower() in re.split("-|/", link) and str(m.team2).lower() in re.split("-|/", link):
+        stats = session.get(link).html.find('.widget-match-stats .content')[0].text
+        possession = '{} - {} | {} - {}'.format(m.team1, stats.split()[1], m.team2, stats.split()[2])
+        shots_on_target = '{} - {} | {} - {}'.format(m.team1, stats.split()[11], m.team2, stats.split()[12])
+        total_passes = '{} - {} | {} - {}'.format(m.team1, stats.split()[15], m.team2, stats.split()[16])
+        print('Possession: {} \nShots on Target: {} \nTotal Passes: {}'.format(possession, shots_on_target, total_passes))
 
